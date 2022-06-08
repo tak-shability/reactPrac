0a6708e319f7f6ef344fbe7bacdd2450dae516e9
tak-shability <tak@shability.io>
2022-06-08 18:22:27 +0900

    CRA, state, component

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,57 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '대구 스파크 가자']);
+    let [따봉, 따봉변경] = useState(0);
+    let posts = '강남 고기 맛집';
+    const 제목바꾸기 = () => {
+        let newArr = [...글제목];
+        newArr[0] = ['여자 코트 추천'];
+        글제목변경(newArr);
+    };
     return (
         <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>hello</p>
-                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
+            <div className='black-nav'>
+                <div>개발 Blog</div>
+            </div>
+            <button onClick={제목바꾸기}>버튼</button>
+            <div className='list'>
+                <h3>
+                    {글제목[0]}
+                    <span
+                        onClick={() => {
+                            따봉변경(따봉 + 1);
+                        }}
+                    >
+                        👍
+                    </span>
+                    {따봉}
+                </h3>
+                <p> 2월 17일 발행</p>
+                <hr />
+                <h3> {글제목[1]} </h3>
+                <p> 2월 18일 발행</p>
+                <hr />
+                <h3> {글제목[2]} </h3>
+                <p> 2월 19일 발행</p>
+                <hr />
+            </div>
+
+            <Modal />
+        </div>
+    );
+}
+
+function Modal() {
+    return (
+        <div className='modal'>
+            <h2>제목</h2>
+            <p>날짜</p>
+            <p>상세내용</p>
         </div>
     );
 }
